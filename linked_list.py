c4616417f2d9d31b379b29d31b7460366122e987
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-05-09 16:11:33 -0300

    Implementei o metodo search em LinkedList e adicionei alguns testes

--- linked_list.py
+++ linked_list.py
@@ -1,37 +1,67 @@
 from node import Node
+
+
+class LinkedListVaziaException(Exception):
+    pass
+
+
 class LinkedList:
     def __init__(self):
-        self.head = None #O inicio da lista
-        self.tail = None #O fim da lista
-        
+        self.head = None  # O inicio da lista
+        self.tail = None  # O fim da lista
+
     def is_empty(self):
-        return self.head == None
+        return not self.head
     
+    def size(self):
+        count = 0
+        curr = self.head
+        while curr is not None:
+            count += 1
+            curr = curr.get_next()
+        return count
+
     def append(self, valor):
         node = Node(valor)
-        if(self.head == None):
+        if (self.head is None):
             self.head = node
             self.tail = node
         else:
             temp = self.tail
             temp.set_next(node)
             self.tail = node
-            
+
     def pop(self):
-        if(self.head == self.tail):
+        if self.head is None:
+            raise LinkedListVaziaException(
+                "Lista vazia! Elementos não podem ser removidos!")
+
+        value_removed = self.head.get_data()
+
+        if self.head.get_next() is None:
             self.head = None
             self.tail = None
         else:
-            temp = self.head.get_next()
-            self.head = temp
+            self.head = self.head.get_next()
+
+        return value_removed
+    
+    def search(self, item):
+        if(self.is_empty()):
+            raise LinkedListVaziaException("Lista vazia!")
         
-        
-    def __str__(self):
         curr = self.head
-        text = ""
-        while curr != None:
-            text += str(curr.get_data())
-            if(curr.get_next() != None):
-                text += " => "
+        while curr is not None:
+            if(curr.get_data() == item):
+                return True
             curr = curr.get_next()
-        return text
+            
+        return False
+
+    def __str__(self):
+        elements = []
+        curr = self.head
+        while curr:
+            elements.append(str(curr.get_data()))
+            curr = curr.get_next()
+        return " => ".join(elements)
--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 from node import Node
 from linked_list import LinkedList
 
-v1 = Node(1)
-v2 = Node(2)
-v3 = Node(3)
-v4 = Node(4)
-v5 = Node(5)
-v6 = Node(6)
-v7 = Node(7)
-v8 = Node(8)
-
 
 l1 = LinkedList()
 print("----- BLOCO 1 -----")
-l1.append(v1)
-l1.append(v2)
-l1.append(v3)
-l1.append(v4)
-l1.append(v5)
-l1.append(v6)
-l1.append(v7)
-l1.append(v8)
+l1.append(1)
+l1.append(2)
+l1.append(3)
+l1.append(4)
+l1.append(5)
+l1.append(6)
+l1.append(7)
+l1.append(8)
 print(l1)
 
 
@@ -29,3 +20,7 @@ l1.pop()
 l1.pop()
 l1.pop()
 print(l1)
+
+print("\n----- BLOCO 3 -----")
+print(l1.search(10))
+print(l1.search(6))
