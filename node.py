1f66771d9cad376a1604ecbf9bae70ca29c016a9
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-05-09 09:51:21 -0300

    Classe Node adicionada

---
+++ node.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self._data = value
+        self._next = None
+        
+    def get_data(self):
+        return self._data
+    
+    def get_next(self):
+        return self._next
+    
+    def set_data(self, new_value):
+        self._data = new_value
+        
+    def set_next(self, updated_next):
+        self._next = updated_next
+        
+    def __str__(self):
+        return f"Node [{str(self.get_data())}] => {str(self.get_next())}"
