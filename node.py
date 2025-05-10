5876bd97625d369d29d378f94a5e144ac41b8e22
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-05-10 13:44:06 -0300

    Adicionei tipos em node

--- node.py
+++ node.py
@@ -1,19 +1,19 @@
 class Node:
     def __init__(self, value):
-        self._data = value
-        self._next = None
-        
-    def get_data(self):
+        self._data: any = value
+        self._next: Node | None = None
+
+    def get_data(self) -> any:
         return self._data
-    
-    def get_next(self):
+
+    def get_next(self) -> "Node | None":
         return self._next
-    
-    def set_data(self, new_value):
+
+    def set_data(self, new_value: any) -> None:
         self._data = new_value
-        
-    def set_next(self, updated_next):
+
+    def set_next(self, updated_next: "Node") -> None:
         self._next = updated_next
-        
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"Node [{str(self.get_data())}]"
