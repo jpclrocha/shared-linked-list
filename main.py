e390d0722d17f258a19ba279aa383e4990cf40d6
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-05-10 13:45:30 -0300

    Removi as adicoes predefinidas e adicionei threads com funcoes workers para adicionarem elementos aleatorios, remover elementos ou buscar elementos aleatorios da lista

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-from node import Node
-from linked_list import LinkedList
+from threading import Thread
+from linked_list import LinkedList, LinkedListVaziaException
+from custom_logger import get_logger
+import random
+from math import floor
 
+logger = get_logger()
+lista = LinkedList()
 
-l1 = LinkedList()
-print("----- BLOCO 1 -----")
-l1.append(1)
-l1.append(2)
-l1.append(3)
-l1.append(4)
-l1.append(5)
-l1.append(6)
-l1.append(7)
-l1.append(8)
-print(l1)
+def worker_append():
+    global lista
+    for _ in range(0, 50):
+        value = floor(random.random() * 10)
+        lista.append(value)
+        logger.debug(f"Adicionando {value} na lista; Estado atual da lista: {lista}")
 
+def worker_pop():
+    global lista
+    for _ in range(0, 60):
+        try:
+            logger.debug(f"Removendo {lista.pop()} da lista; Estado atual da lista: {lista}")
+        except LinkedListVaziaException:
+            logger.debug("Lista Vazia! Elementos não podem ser removidos!")
+        
+def worker_search():
+    global lista
+    for _ in range(0, 50):
+        try:
+            value = floor(random.random() * 10)
+            logger.debug(f"Procurando {value} na lista; Encontrou? {'Sim' if lista.search(value) else 'Não'}; Estado atual da lista: {lista}")
+        except LinkedListVaziaException:
+            logger.debug("Lista Vazia! Elementos não será encontrado!")
 
-print("\n----- BLOCO 2 -----")
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-l1.pop()
-print(l1)
+# create threads
+t1 = Thread(target=worker_append, name="ADD-1")
+t2 = Thread(target=worker_append, name="ADD-2")
+t3 = Thread(target=worker_pop, name="POP-1")
+t4 = Thread(target=worker_pop, name="POP-2")
+t5 = Thread(target=worker_search, name="BUSCA")
 
-print("\n----- BLOCO 3 -----")
-print(l1.search(10))
-print(l1.search(6))
+# start the threads
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+
+# wait for the threads to complete
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+
+print(lista)
