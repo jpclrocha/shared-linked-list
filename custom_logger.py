a23895ccac75a29e3e7dd9f9a2e38cf4d113e09b
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-05-10 13:46:15 -0300

    Criei um logger para conseguir diferenciar as threads no momento de execucao, cada thread possui um nome e uma cor

---
+++ custom_logger.py
@@ -0,0 +1,27 @@
+import logging
+
+# ANSI cores
+COLORS = {
+    "ADD-1": "\033[91m",  # vermelho
+    "ADD-2": "\033[92m",  # verde
+    "POP-1": "\033[94m",  # azul
+    "POP-2": "\033[93m",  # amarelo
+    "BUSCA": "\033[1;35m",  # magenta
+}
+RESET = "\033[0m"
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record):
+        color = COLORS.get(record.threadName, "")
+        record.msg = f"{color}{record.msg}{RESET}"
+        return super().format(record)
+
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(ColorFormatter("[%(threadName)s] %(message)s"))
+    logger.handlers = [handler]
+    
+    return logger
